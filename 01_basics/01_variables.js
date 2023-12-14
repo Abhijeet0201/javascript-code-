8521d40eb94f0a255dd75626cbff188e0f0b3e75
Abhijeet0201 <kumarsinghabhijeet572@gmail.com>
2023-12-14 15:13:27 +0000

    container confog done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,2 +1,19 @@
 const accountId =1234
 let accountEmail ="abhijeet@ gmail.com"
+var accountPassword = "15446"
+accountCity ="jaipur"
+
+//accountId =1 //not chanege allowed
+accountEmail = "abcde@gmail.com"
+accountPassword ="5452584"
+accountCity = "Patna" 
+let accountState;
+console.log(accountId);
+
+/*
+prefer not use var
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
