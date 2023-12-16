ee3be36ff835090ab64b928ca14ba3814b4653ae
Abhijeet0201 <kumarsinghabhijeet572@gmail.com>
2023-12-16 13:27:43 +0000

    dataTypes

---
+++ 01_basics/01_dataType.js
@@ -0,0 +1,20 @@
+ "use strict"; //is used for treat all js as newer version.
+ //alert(3+3) // we are using nodejs ,not browser
+ // js tere is not many data type like other language 
+ let  name ="sonu"
+ let age = 20
+ let isloging = false
+
+ // number = 2 to power 53
+ // bigint
+ //String
+ //boolean => true /false
+ // null ==> standalone 
+ //undefine =
+ //Symbol => uniqe
+  
+ // the above data type is primitive data type
+
+ // object
+ console.log(typeof undefined );// undefined
+ console.log(typeof null); // it object 
