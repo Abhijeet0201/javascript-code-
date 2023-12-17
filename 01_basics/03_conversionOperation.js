03a09d33348872688723584de74ec1a70871efd1
Abhijeet0201 <kumarsinghabhijeet572@gmail.com>
2023-12-17 07:33:16 +0000

    conversion of datatype

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -20,5 +20,36 @@ let booleanIsLoggedIn = Boolean(isloggedIn)
 
  let someNumber = 54
  let stringNumber = String(someNumber);
- console.log(stringNumber);
- console.log( typeof stringNumber );
+ //console.log(stringNumber);
+ //console.log( typeof stringNumber );
+ //***************** Operation ******************
+ let value =3
+ let negvalue = -value
+ //console.log();
+
+ //console.log(2+2);
+ //console.log(2-2);
+ //console.log(2*2);
+ //console.log(2**2);
+ //console.log(2/2);
+ //console.log(2%2)
+
+ let str1 = "hello"
+ let str2 = "sonu"
+
+ let str3 = str1 + str2
+// console.log(str3);
+
+ //console.log("1"+2);
+ //console.log(1+"2");
+ //console.log("1"+2+2);
+ //console.log(1+2+"2");
+
+// console.log((3+4) *5 %3);
+ //console.log(+true);
+// console.log(+"");
+
+let num1,num2 , num3 
+let  gamecounter =120
+++gamecounter
+console.log(gamecounter);
