386926ef4e14fa6079c75305bbb4fb6547bfc174
Abhijeet0201 <kumarsinghabhijeet572@gmail.com>
2023-12-17 06:19:30 +0000

    conversion of datatype

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,16 +1,24 @@
 let score ="54adf"
-console.log(typeof score);
+//console.log(typeof score);
 // or write in this type
-console.log(typeof (score));
+//console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log( typeof valueInNumber);
-console.log( valueInNumber);  
+//console.log( typeof valueInNumber);
+//console.log( valueInNumber);  
 
 // "54" => 33
 //"54adf" =>NaN
 // true => 1; false =>0;
-let isloggedIn =1
+let isloggedIn ="sonu"
 
 let booleanIsLoggedIn = Boolean(isloggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
+// 1=> true; 0=> false
+// "" =>false
+//"sonu" =>true
+
+ let someNumber = 54
+ let stringNumber = String(someNumber);
+ console.log(stringNumber);
+ console.log( typeof stringNumber );
