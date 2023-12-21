e5885217dbb957186d830858bb4977e0d291221c
Abhijeet0201 <kumarsinghabhijeet572@gmail.com>
2023-12-21 15:14:57 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,7 @@
+const myName = "Abhijeet"
+const accountId = 50
+
+// console.log(myName + accountId + "value");
+console.log(`Hello my name is ${myName} and my account id is ${accountId }`); // its written in backtick method is sting interpolation
+
+const gameName = new String('sonu')
