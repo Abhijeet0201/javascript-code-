abe236ce0a1eb1119e12384370c54cfe225ff1a3
Abhijeet0201 <kumarsinghabhijeet572@gmail.com>
2023-12-29 15:03:47 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,29 @@
+// singleton ->koi v const se banate hai to singleton banta hai.
+// Object.create -> ye hai singleton ,aise v object banta hai or ise constrution method hai.
+
+//object literals
+
+const mySym  = Symbol("mykey1")
+
+
+const jsUser= {
+    name:"Abhijeet",
+    fullName:"Abhijeet Kumar",
+    [mySym]: "mykey1", // symbole use karne kele ham [] used karte hai. nahi to ye symbole nahi lega.
+    age:21,
+    location: "Pagwara",
+    email:"sonu@gmail.com",
+    isLoggedIn:false,
+    lastLogingDay: ["monday" , "Thrusday"]
+}
+//console.log(jsUser.email); //yaha ye String kude le ta hai yaha .
+//console.log(jsUser);
+//console.log(jsUser["email"]);// ya par nahi leta to " " to yea use karte hai.
+//console.log(jsUser.mySym);// line 6 wala lea raha hai hai
+console.log(typeof jsUser.mySym);
+console.log(jsUser[mySym]);// ye line 12 ko print kar waraha hai.
+
+jsUser.email = "sonukumar@gmail.com"// use for changing
+Object.freeze(jsUser);// ye used karne baad kab koi change nahi hoga.
+jsUser.email = "sonukumar@gmail.com"// example-> ke lea yea ab change nahi hoga.freeze ke baad
+console.log(jsUser); 
