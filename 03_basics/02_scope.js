f9d730ec1425986aebc778fc38a517ef3226becd
Abhijeet0201 <kumarsinghabhijeet572@gmail.com>
2023-12-31 15:03:49 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,22 @@
+
+if(true){
+    let a =10;
+    const b= 20
+    var c =30
+}
+
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+function one(){
+    const username ="Abhijeet"
+    function two() {
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website);
+
+    two()
+}
+one()
