604d5db5ae93a74714eaf059ce8c0313f70cb9e4
Abhijeet0201 <kumarsinghabhijeet572@gmail.com>
2024-01-03 14:48:51 +0000

    Switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,55 @@
+/*switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+*/
+/*const month = 3 // for number
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+
+    default:
+        console.log("defalt");
+        break;
+}
+*/
+
+const month = "May" // for String
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "Feb":
+        console.log("February");
+        break;
+    case "March":
+        console.log("March");
+        break;
+    case "April":
+        console.log("April");
+        break;
+    case "May":
+        console.log("May");
+        break;
+
+    default:
+        console.log("defalt");
+        break;
+}
