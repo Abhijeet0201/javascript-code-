6777cc51438e99460ac9442898556afdddb1c8cb
Abhijeet0201 <kumarsinghabhijeet572@gmail.com>
2024-01-03 14:49:26 +0000

    truthy

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,39 @@
+/*const userEmail ="Abhijeet@gmail.com"
+
+if (userEmail) {
+    console.log("Get email");
+} else {
+   console.log("Dont get email"); 
+}*/
+
+// falsy values
+//false, 0,-0, BigInt 0n,"",'',null, undefined, NaN
+
+//truthy values
+// "0",'false',"false"," ",[],{},function(){}
+
+/*const userEmail = []
+if (userEmail.length ===0) {
+    console.log("Array is empty");
+
+}*/
+const emptyObj = {}
+if (Object.keys(emptyObj).length ===0) {
+    console.log("Object is empty");
+}
+
+//Nullish coalescing Operator (??) : null ,undefined
+
+let val1;
+//val1 = 5??10
+//val1 = null ?? 10
+//val1 = 5 ?? null
+//val1 = undefined ?? 5
+val1 = null ?? 10 ?? 5
+
+console.log(val1);
+
+// terniary Operator
+//Syntax => (condition) ? true : false
+const iceTeaPrice = 100
+iceTeaPrice <=80 ? console.log("less than 80") : console.log('More than 80');
