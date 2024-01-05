39a4ca3682d88280548c1cdae56bed74d860f770
Abhijeet0201 <kumarsinghabhijeet572@gmail.com>
2024-01-05 17:57:56 +0000

    four

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -6,7 +6,39 @@
    // console.log(val);
  //})
 
- coding.forEach((iteam) => {
+ /*coding.forEach((iteam) => {
     console.log(iteam);    
  });
- 
+
+ function printMe(iteam){
+  console.log(iteam);
+ }
+ coding.forEach(printMe)
+ */
+
+//coding.forEach((item, index, arr) => {
+  //console.log(item, index, arr);
+//})
+
+const myCoding= [
+  {
+    languageName : "javascript",
+    languageFile :"js"
+  },
+  {
+    languageName : "python",
+    languageFile : "Py"
+  },
+  {
+    languageName : "Java",
+    LanguageFile : "java"
+  }
+
+]
+function printMe(iteam,value) {
+  console.log(iteam,value);
+}
+myCoding.forEach(printMe);
+//myCoding.forEach((iteam) =>{
+ // console.log(iteam.languageName);
+//})
