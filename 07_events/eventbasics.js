9dfd4fbf5da05140406b235fcf238055787bfd5d
Abhijeet0201 <kumarsinghabhijeet572@gmail.com>
2024-01-18 14:38:45 +0000

    eventbasics

---
+++ 07_events/eventbasics.js
@@ -0,0 +1,7 @@
+/*function logClickTime() {
+    var logElement = document.getElementById('log');
+    var currentDateTime = new Date().toLocaleString();
+    var logText = 'Clicked at: ' + currentDateTime;
+    
+    logElement.textContent = logText;
+}*/
