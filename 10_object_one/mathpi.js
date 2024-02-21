e37159d61066a5071b69a6c35a3d971594e96710
Abhijeet0201 <kumarsinghabhijeet572@gmail.com>
2024-02-21 14:54:49 +0000

    Mathpi

---
+++ 10_object_one/mathpi.js
@@ -0,0 +1,17 @@
+const descriptor = Object.getOwnPropertyDescriptor(Math,"PI")
+
+//console.log(descriptor);
+
+const chai ={
+    name: 'masala chai',
+    price: 100,
+    isAvailable: true
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+
+//Object.defineProperty(chai,'name',{
+    //writable: false,
+    //enumerable: false
+//})
+//console.log(Object.getOwnPropertyDescriptor(chai,"name"));
